41d2fe0935a6f273d37b6f56f8c269567ee2ced6
Christopher Kim <christopherjkim@gmail.com>
2012-10-02 10:26:15 -0400

    Oops, this cfg variable is irrelevant now.

--- cfg/cfg.js
+++ cfg/cfg.js
@@ -1,7 +1,5 @@
 module.exports = {
   hosturl: 'http://206.253.166.149/',
   // Sessions are valid for only 3 hours
-  statstimeout: 3*60*60*1000,
-  //refresh stale avatars older than 5 minutes (for testing)
-  avatarrefresh: 5*60*1000
+  statstimeout: 3*60*60*1000
 };
