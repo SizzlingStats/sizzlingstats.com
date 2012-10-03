79449cc8572782d26fbcf8cd4e81d19a7da6a27e
Christopher Kim <christopherjkim@gmail.com>
2012-10-02 22:28:56 -0400

    Added asset management

--- app.js
+++ app.js
@@ -28,6 +28,73 @@ app.configure(function(){
   });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+
+
+  var assetManager = require('connect-assetmanager')({
+    js: {
+      route: /\/js\/all-[a-z0-9]+\.js/,
+      path: __dirname + '/public/js/',
+      dataType: 'javascript',
+      debug: process.env.NODE_ENV === 'development',
+      // preManipulate: {
+      //   '^': [
+      //     function(src, path, index, isLast, callback) {
+      //       callback(src.replace(/#socketIoPort#/g, env.port));
+      //     }
+      //     , function(src, path, index, isLast, callback) {
+      //       if (/\.coffee$/.test(path)) {
+      //         callback(coffee.compile(src));
+      //       } else {
+      //         callback(src);
+      //       }
+      //     }
+      //   ]
+      // },
+      files: [ // order matters here
+        'lib/jquery/jquery-1.8.2.min.js',
+        'lib/angular/angular.js',
+        'lib/bootstrap/bootstrap.min.js',
+        'app.js',
+        'services.js',
+        'controllers.js',
+        'filters.js',
+        'directives.js'
+        // '*'
+      ]
+    },
+    css: {
+      route: /\/css\/all-[a-z0-9]+\.css/,
+      path: __dirname + '/public/css/',
+      dataType: 'css',
+      debug: process.env.NODE_ENV === 'development',
+      // preManipulate: {
+      //   '^': [
+      //     function(src, path, index, isLast, callback) {
+      //       if (/\.styl$/.test(path)) {
+      //         stylus(src)
+      //           .set('compress', false)
+      //           .set('filename', path)
+      //           .set('paths', [ __dirname, app.paths.public ])
+      //           .render(function(err, css) {
+      //             callback(err || css);
+      //           });
+      //       } else {
+      //         callback(src);
+      //       }
+      //     }
+      //   ]
+      // },
+      files: [ // order matters here
+        'bootstrap.css',
+        'bootstrap-fix.css',
+        'bootstrap-responsive.css',
+        'sizzlingstats.css'
+      ]
+    }
+  });
+  app.use(assetManager);
+  app.helpers({ assetManager: assetManager });
+
   app.use(express.static(__dirname + '/public'));
   app.use(app.router);
 });
