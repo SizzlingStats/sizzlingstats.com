a150aecd76fd61a828b62130cf2e530cfae6239f
Christopher Kim <christopherjkim@gmail.com>
2012-12-17 04:43:39 -0500

    Upgraded to Foundation 2.0.3, tweaked CSS.

--- public/js/lib/foundation/jquery.foundation.buttons.js
+++ public/js/lib/foundation/jquery.foundation.buttons.js
@@ -4,16 +4,18 @@
   $.fn.foundationButtons = function (options) {
     var $doc = $(document),
       config = $.extend({
-        dropdownAsToggle:true,
+        dropdownAsToggle:false,
         activeClass:'active'
       }, options),
 
     // close all dropdowns except for the dropdown passed
       closeDropdowns = function (dropdown) {
+        // alert(dropdown.html());
         $('.button.dropdown').find('ul').not(dropdown).removeClass('show-dropdown');
       },
     // reset all toggle states except for the button passed
       resetToggles = function (button) {
+        // alert(button.html());
         var buttons = $('.button.dropdown').not(button);
         buttons.add($('> span.' + config.activeClass, buttons)).removeClass(config.activeClass);
       };
@@ -37,13 +39,15 @@
         }
 
       // close other dropdowns
-      closeDropdowns(config.dropdownAsToggle ? dropdown : '');
-      dropdown.toggleClass('show-dropdown');
+      setTimeout(function () {
+        closeDropdowns(config.dropdownAsToggle ? dropdown : '');
+        dropdown.toggleClass('show-dropdown');
 
-      if (config.dropdownAsToggle) {
-        resetToggles(button);
-        $el.toggleClass(config.activeClass);
-      }
+        if (config.dropdownAsToggle) {
+          resetToggles(button);
+          $el.toggleClass(config.activeClass);
+        }
+      }, 0);
     });
 
     // close all dropdowns and deactivate all buttons
@@ -59,10 +63,10 @@
     });
 
     // Positioning the Flyout List
-    var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny)', this).outerHeight() - 1,
-        largeButtonHeight   = $('.button.large.dropdown', this).outerHeight() - 1,
-        smallButtonHeight   = $('.button.small.dropdown', this).outerHeight() - 1,
-        tinyButtonHeight    = $('.button.tiny.dropdown', this).outerHeight() - 1;
+    var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny):visible', this).outerHeight() - 1,
+        largeButtonHeight   = $('.button.large.dropdown:visible', this).outerHeight() - 1,
+        smallButtonHeight   = $('.button.small.dropdown:visible', this).outerHeight() - 1,
+        tinyButtonHeight    = $('.button.tiny.dropdown:visible', this).outerHeight() - 1;
 
     $('.button.dropdown:not(.large):not(.small):not(.tiny) > ul', this).css('top', normalButtonHeight);
     $('.button.dropdown.large > ul', this).css('top', largeButtonHeight);
