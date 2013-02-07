801fea39e10a68db7f52cdc8e13057def4262b38
Christopher Kim <christopherjkim@gmail.com>
2013-02-07 03:19:55 -0500

    Added mocks for testing stats api.

---
+++ tests/test-api-mocks.js
@@ -0,0 +1,202 @@
+/**
+ * tests/test-api-mocks.js
+ * Mock request data
+ */
+
+    // 0 -> 0
+    // 1 -> 1 Scout
+    // 2 -> 8 Sniper
+    // 3 -> 2 Soldier
+    // 4 -> 4 Demoman
+    // 5 -> 7 Medic
+    // 6 -> 5 Heavy
+    // 7 -> 3 Pyro
+    // 8 -> 9 Spy
+    // 9 -> 6 Engineer
+
+    // PLAYED_SCOUT    = 1<<0; //   1          1  should be:          1
+    // PLAYED_SNIPER   = 1<<1; //   2         10  should be:   10000000
+    // PLAYED_SOLDIER  = 1<<2; //   4        100  should be:         10
+    // PLAYED_DEMOMAN  = 1<<3; //   8       1000  should be:       1000
+    // PLAYED_MEDIC    = 1<<4; //  16      10000  should be:    1000000
+    // PLAYED_HEAVY    = 1<<5; //  32     100000  should be:      10000
+    // PLAYED_PYRO     = 1<<6; //  64    1000000  should be:        100
+    // PLAYED_SPY      = 1<<7; // 128   10000000  should be:  100000000
+    // PLAYED_ENGINEER = 1<<8; // 256  100000000  should be:     100000
+
+module.exports = {
+
+  statsHeaders1: {
+    "sizzlingstats": "v0.1"
+  }
+
+, statsBody1: {"stats": {
+    "map": "cp_badlands"
+  , "hostname": "heyyyyy this is a pretty long title"
+  , "bluname": "BluName"
+  , "redname": "RedName"
+  , "players": [
+      {"steamid": "STEAM_0:0:86144",    "team": 2, "name": "Cigar",       "mostplayedclass": 1}
+    , {"steamid": "STEAM_0:1:17260233", "team": 3, "name": "SteveC",      "mostplayedclass": 1}
+    , {"steamid": "STEAM_0:1:16097990", "team": 2, "name": "foster",      "mostplayedclass": 1}
+    , {"steamid": "STEAM_0:0:7422703",  "team": 3, "name": "binarystar!", "mostplayedclass": 1}
+    , {"steamid": "STEAM_0:0:17990207", "team": 2, "name": "techno",      "mostplayedclass": 3}
+    , {"steamid": "STEAM_0:1:11029950", "team": 3, "name": "rando",       "mostplayedclass": 3}
+    , {"steamid": "STEAM_0:0:14353663", "team": 2, "name": "Sizzling",    "mostplayedclass": 3}
+    , {"steamid": "STEAM_0:1:9607258",  "team": 3, "name": "Dave__AC",    "mostplayedclass": 3}
+    , {"steamid": "STEAM_0:1:19111546", "team": 2, "name": "drdonutman",  "mostplayedclass": 4}
+    , {"steamid": "STEAM_0:0:21274546", "team": 3, "name": "Gobiner",     "mostplayedclass": 4}
+    , {"steamid": "STEAM_0:0:9038326",  "team": 2, "name": "Trekkie",     "mostplayedclass": 5}
+    , {"steamid": "STEAM_0:0:5486374",  "team": 3, "name": "eXtine",      "mostplayedclass": 5}
+    // , {"steamid": "STEAM_0:1:19550627", "team": 3, "name": "Jay",         "mostplayedclass": 5}
+    ]
+  }}
+
+
+, statsHeaders2: {
+    "sizzlingstats": "v0.1"
+  , "endofround": "false"
+  , "sessionid": "something"
+  }
+
+, statsBody2: {"stats": {
+    "map": "YouShouldNotSeeThis"
+  , "hostname": "YouShouldNotSeeThis"
+  , "bluname": "YouShouldNotSeeThis"
+  , "redname": "YouShouldNotSeeThis"
+  , "bluscore": 0
+  , "redscore": 0
+  , "roundduration": 100
+  , "players": [
+      {"steamid": "STEAM_0:0:86144",    "team": 2, "name": "Cigar",       "mostplayedclass": 1, "playedclasses": 0,   "kills": 0, "deaths":2}
+    , {"steamid": "STEAM_0:1:17260233", "team": 3, "name": "SteveC",      "mostplayedclass": 1, "playedclasses": 3,   "kills": 0, "deaths":8}
+    , {"steamid": "STEAM_0:1:16097990", "team": 2, "name": "foster",      "mostplayedclass": 1, "playedclasses": 1,   "kills": 2, "deaths":4}
+    , {"steamid": "STEAM_0:0:7422703",  "team": 3, "name": "binarystar!", "mostplayedclass": 1, "playedclasses": 35,  "kills": 9, "deaths":9}
+    , {"steamid": "STEAM_0:0:17990207", "team": 2, "name": "techno",      "mostplayedclass": 3, "playedclasses": 4,   "kills": 8, "deaths":4}
+    , {"steamid": "STEAM_0:1:11029950", "team": 3, "name": "rando",       "mostplayedclass": 3, "playedclasses": 4,   "kills": 4, "deaths":2}
+    , {"steamid": "STEAM_0:0:14353663", "team": 2, "name": "Sizzling",    "mostplayedclass": 3, "playedclasses": 4,   "kills": 2, "deaths":2}
+    , {"steamid": "STEAM_0:1:9607258",  "team": 3, "name": "Dave__AC",    "mostplayedclass": 3, "playedclasses": 4,   "kills": 1, "deaths":4}
+    , {"steamid": "STEAM_0:1:19111546", "team": 2, "name": "drdonutman",  "mostplayedclass": 4, "playedclasses": 8,   "kills": 1, "deaths":5}
+    , {"steamid": "STEAM_0:0:21274546", "team": 3, "name": "Gobiner",     "mostplayedclass": 4, "playedclasses": 323, "kills": 3, "deaths":1}
+    , {"steamid": "STEAM_0:0:9038326",  "team": 2, "name": "Trekkie",     "mostplayedclass": 5, "playedclasses": 16 , "kills": 4, "deaths":6}
+    , {"steamid": "STEAM_0:0:5486374",  "team": 3, "name": "eXtine",      "mostplayedclass": 5, "playedclasses": 16 , "kills": 7, "deaths":1}
+    // , {"steamid": "STEAM_0:1:19550627", "team": 3, "name": "Jay",         "mostplayedclass": 5, "playedclasses": 16, "kills": 3, "deaths":0}
+    ]
+  }}
+
+
+, statsHeaders3: {
+    "sizzlingstats": "v0.1"
+  , "endofround": "false"
+  , "sessionid": "something"
+  }
+
+, statsBody3: {"stats": {
+    "map": "YouShouldNotSeeThis"
+  , "hostname": "YouShouldNotSeeThis"
+  , "bluname": "YouShouldNotSeeThis"
+  , "redname": "YouShouldNotSeeThis"
+  , "bluscore": 0
+  , "redscore": 0
+  , "roundduration": 120
+  , "players": [
+      {"steamid": "STEAM_0:0:86144",    "team": 2, "name": "Cigar",       "mostplayedclass": 1, "playedclasses": 0,   "kills": 0, "deaths":2}
+    , {"steamid": "STEAM_0:1:17260233", "team": 3, "name": "SteveC",      "mostplayedclass": 1, "playedclasses": 3,   "kills": 0, "deaths":8}
+    , {"steamid": "STEAM_0:1:16097990", "team": 2, "name": "foster",      "mostplayedclass": 1, "playedclasses": 1,   "kills": 2, "deaths":4}
+    , {"steamid": "STEAM_0:0:7422703",  "team": 3, "name": "binarystar!", "mostplayedclass": 1, "playedclasses": 35,  "kills": 9, "deaths":9}
+    , {"steamid": "STEAM_0:0:17990207", "team": 2, "name": "techno",      "mostplayedclass": 3, "playedclasses": 4,   "kills": 8, "deaths":4}
+    , {"steamid": "STEAM_0:1:11029950", "team": 3, "name": "rando",       "mostplayedclass": 3, "playedclasses": 4,   "kills": 4, "deaths":2}
+    , {"steamid": "STEAM_0:0:14353663", "team": 2, "name": "Sizzling",    "mostplayedclass": 3, "playedclasses": 4,   "kills": 2, "deaths":2}
+    , {"steamid": "STEAM_0:1:9607258",  "team": 3, "name": "Dave__AC",    "mostplayedclass": 3, "playedclasses": 4,   "kills": 1, "deaths":4}
+    , {"steamid": "STEAM_0:1:19111546", "team": 2, "name": "drdonutman",  "mostplayedclass": 4, "playedclasses": 8,   "kills": 1, "deaths":5}
+    , {"steamid": "STEAM_0:0:21274546", "team": 3, "name": "Gobiner",     "mostplayedclass": 4, "playedclasses": 323, "kills": 3, "deaths":1}
+    , {"steamid": "STEAM_0:0:9038326",  "team": 2, "name": "Trekkie",     "mostplayedclass": 5, "playedclasses": 16 , "kills": 4, "deaths":6}
+    , {"steamid": "STEAM_0:0:5486374",  "team": 3, "name": "eXtine",      "mostplayedclass": 5, "playedclasses": 16 , "kills": 7, "deaths":1}
+    , {"steamid": "STEAM_0:1:19550627", "team": 3, "name": "Jay",         "mostplayedclass": 5, "playedclasses": 16, "kills": 3, "deaths":0}
+    ]
+  }}
+
+
+, statsHeaders4: {
+    "sizzlingstats": "v0.1"
+  , "endofround": "true"
+  , "sessionid": "something"
+  }
+
+, statsBody4: {"stats": {
+    "map": "YouShouldNotSeeThis"
+  , "hostname": "YouShouldNotSeeThis"
+  , "bluname": "YouShouldNotSeeThis"
+  , "redname": "YouShouldNotSeeThis"
+  , "bluscore": 0
+  , "redscore": 1
+  , "roundduration": 160
+  , "players": [
+      {"steamid": "STEAM_0:0:86144",    "team": 2, "name": "Cigar",       "mostplayedclass": 1, "playedclasses": 0,   "kills": 0, "deaths":2}
+    , {"steamid": "STEAM_0:1:17260233", "team": 3, "name": "SteveC",      "mostplayedclass": 1, "playedclasses": 3,   "kills": 0, "deaths":8}
+    , {"steamid": "STEAM_0:1:16097990", "team": 2, "name": "foster",      "mostplayedclass": 1, "playedclasses": 1,   "kills": 2, "deaths":4}
+    , {"steamid": "STEAM_0:0:7422703",  "team": 3, "name": "binarystar!", "mostplayedclass": 1, "playedclasses": 35,  "kills": 9, "deaths":9}
+    , {"steamid": "STEAM_0:0:17990207", "team": 2, "name": "techno",      "mostplayedclass": 3, "playedclasses": 4,   "kills": 8, "deaths":4}
+    , {"steamid": "STEAM_0:1:11029950", "team": 3, "name": "rando",       "mostplayedclass": 3, "playedclasses": 4,   "kills": 4, "deaths":2}
+    , {"steamid": "STEAM_0:0:14353663", "team": 2, "name": "Sizzling",    "mostplayedclass": 3, "playedclasses": 4,   "kills": 2, "deaths":2}
+    , {"steamid": "STEAM_0:1:9607258",  "team": 3, "name": "Dave__AC",    "mostplayedclass": 3, "playedclasses": 4,   "kills": 1, "deaths":4}
+    , {"steamid": "STEAM_0:1:19111546", "team": 2, "name": "drdonutman",  "mostplayedclass": 4, "playedclasses": 8,   "kills": 1, "deaths":5}
+    , {"steamid": "STEAM_0:0:21274546", "team": 3, "name": "Gobiner",     "mostplayedclass": 4, "playedclasses": 323, "kills": 3, "deaths":1}
+    , {"steamid": "STEAM_0:0:9038326",  "team": 2, "name": "Trekkie",     "mostplayedclass": 5, "playedclasses": 16 , "kills": 4, "deaths":6}
+    , {"steamid": "STEAM_0:0:5486374",  "team": 3, "name": "eXtine",      "mostplayedclass": 5, "playedclasses": 16 , "kills": 7, "deaths":1}
+    // , {"steamid": "STEAM_0:1:19550627", "team": 3, "name": "Jay",         "mostplayedclass": 5, "playedclasses": 16, "kills": 3, "deaths":0}
+    ]
+  }}
+
+
+, statsHeaders5: {
+    "sizzlingstats": "v0.1"
+  , "endofround": "true"
+  , "sessionid": "something"
+  }
+
+, statsBody5: {"stats": {
+    "map": "YouShouldNotSeeThis"
+  , "hostname": "YouShouldNotSeeThis"
+  , "bluname": "YouShouldNotSeeThis"
+  , "redname": "YouShouldNotSeeThis"
+  , "bluscore": 1
+  , "redscore": 1
+  , "roundduration": 160
+  , "players": [
+      {"steamid": "STEAM_0:0:86144",    "team": 2, "name": "Cigar",       "mostplayedclass": 1, "playedclasses": 0,   "kills": 0, "deaths":2}
+    , {"steamid": "STEAM_0:1:17260233", "team": 3, "name": "SteveC",      "mostplayedclass": 1, "playedclasses": 3,   "kills": 0, "deaths":8}
+    , {"steamid": "STEAM_0:1:16097990", "team": 2, "name": "foster",      "mostplayedclass": 1, "playedclasses": 1,   "kills": 2, "deaths":4}
+    , {"steamid": "STEAM_0:0:7422703",  "team": 3, "name": "binarystar!", "mostplayedclass": 1, "playedclasses": 35,  "kills": 9, "deaths":9}
+    , {"steamid": "STEAM_0:0:17990207", "team": 2, "name": "techno",      "mostplayedclass": 3, "playedclasses": 4,   "kills": 8, "deaths":4}
+    , {"steamid": "STEAM_0:1:11029950", "team": 3, "name": "rando",       "mostplayedclass": 3, "playedclasses": 4,   "kills": 4, "deaths":2}
+    , {"steamid": "STEAM_0:0:14353663", "team": 2, "name": "Sizzling",    "mostplayedclass": 3, "playedclasses": 4,   "kills": 2, "deaths":2}
+    , {"steamid": "STEAM_0:1:9607258",  "team": 3, "name": "Dave__AC",    "mostplayedclass": 3, "playedclasses": 4,   "kills": 1, "deaths":4}
+    , {"steamid": "STEAM_0:1:19111546", "team": 2, "name": "drdonutman",  "mostplayedclass": 4, "playedclasses": 8,   "kills": 1, "deaths":5}
+    , {"steamid": "STEAM_0:0:21274546", "team": 3, "name": "Gobiner",     "mostplayedclass": 4, "playedclasses": 323, "kills": 3, "deaths":1}
+    , {"steamid": "STEAM_0:0:9038326",  "team": 2, "name": "Trekkie",     "mostplayedclass": 5, "playedclasses": 16 , "kills": 4, "deaths":6}
+    , {"steamid": "STEAM_0:0:5486374",  "team": 3, "name": "eXtine",      "mostplayedclass": 5, "playedclasses": 16 , "kills": 7, "deaths":1}
+    // , {"steamid": "STEAM_0:1:19550627", "team": 3, "name": "Jay",         "mostplayedclass": 5, "playedclasses": 16, "kills": 3, "deaths":0}
+    , {"steamid": "STEAM_0:1:5201690",  "team": 2, "name": "b4nny",       "mostplayedclass": 4, "playedclasses": 8,   "kills": 9, "deaths":1}
+    ]
+  }}
+
+
+, statsHeaders6: {
+    "sizzlingstats": "v0.1"
+  , "sessionid": "something"
+  , "matchduration": "222"
+  }
+
+, statsBody6: {"chats": [
+    { "steamid": "STEAM_0:0:14353663"
+    , "isTeam": 0
+    , "time": 156
+    , "message": "\"My name is SizzlingCalamari. Goodbye world!\"" }
+  , { "steamid": "STEAM_0:0:7422703"
+    , "isTeam": 0
+    , "time": 161
+    , "message": "BINARYSTAR SAYS: THIS IS GOODBYE" }
+  ]}
+
+};
---
+++ tests/test-api.js
@@ -0,0 +1,237 @@
+/**
+ * tests/test-api.js
+ */
+
+var assert = require('assert')
+  , async = require('async')
+  , request = require('request')
+  , mock = require('./test-api-mocks.js');
+
+
+var sessionid, matchurl;
+
+var optionsNew = {
+  uri: 'http://localhost:8001/api/stats/new',
+  method: 'POST',
+  timeout: 7000
+};
+
+var optionsUpdate = {
+  uri: 'http://localhost:8001/api/stats/update',
+  method: 'POST',
+  timeout: 7000
+};
+
+var optionsGameover = {
+  uri: 'http://localhost:8001/api/stats/gameover',
+  method: 'POST',
+  timeout: 7000
+};
+
+
+/**
+ * Test 1: Mock createStats
+ */
+
+var test1 = function(callback) {
+
+  optionsNew.headers = mock.statsHeaders1;
+  optionsNew.json = mock.statsBody1;
+
+  request(optionsNew, function(err, res, body) {
+    // Error checking
+    if (err) {
+      return callback(err);
+    }
+    if (res.statusCode !== 200) {
+      return callback(new Error('tests/test-api.js -API Error: ' + res.statusCode));
+    }
+
+    sessionid = res.headers.sessionid;
+    matchurl = res.headers.matchurl;
+
+    return callback(null, res, body);
+  });
+};
+
+/**
+ * Test 2: Mock updateStats, endofround=false
+ */
+
+var test2 = function(callback) {
+
+  mock.statsHeaders2.sessionid = sessionid;
+  optionsUpdate.headers = mock.statsHeaders2;
+  optionsUpdate.json = mock.statsBody2;
+
+  request(optionsUpdate, function(err, res, body) {
+    // Error checking
+    if (err) {
+      return callback(err);
+    }
+    if (res.statusCode !== 200) {
+      return callback(new Error('tests/test-api.js -API Error: ' + res.statusCode));
+    }
+
+    return callback(null, res, body);
+  });
+};
+
+/**
+ * Test 3: Mock updateStats, endofround=false, new player added (Jay)
+ */
+
+var test3 = function(callback) {
+
+  mock.statsHeaders3.sessionid = sessionid;
+  optionsUpdate.headers = mock.statsHeaders3;
+  optionsUpdate.json = mock.statsBody3;
+
+  request(optionsUpdate, function(err, res, body) {
+    // Error checking
+    if (err) {
+      return callback(err);
+    }
+    if (res.statusCode !== 200) {
+      return callback(new Error('tests/test-api.js -API Error: ' + res.statusCode));
+    }
+
+    return callback(null, res, body);
+  });
+};
+
+/**
+ * Test 4: Mock updateStats, endofround=true
+ */
+
+var test4 = function(callback) {
+
+  mock.statsHeaders4.sessionid = sessionid;
+  optionsUpdate.headers = mock.statsHeaders4;
+  optionsUpdate.json = mock.statsBody4;
+
+  request(optionsUpdate, function(err, res, body) {
+    // Error checking
+    if (err) {
+      return callback(err);
+    }
+    if (res.statusCode !== 200) {
+      return callback(new Error('tests/test-api.js -API Error: ' + res.statusCode));
+    }
+
+    return callback(null, res, body);
+  });
+};
+
+/**
+ * Test 5: Mock updateStats, endofround=true, new player added (b4nny)
+ */
+
+var test5 = function(callback) {
+
+  mock.statsHeaders5.sessionid = sessionid;
+  optionsUpdate.headers = mock.statsHeaders5;
+  optionsUpdate.json = mock.statsBody5;
+
+  request(optionsUpdate, function(err, res, body) {
+    // Error checking
+    if (err) {
+      return callback(err);
+    }
+    if (res.statusCode !== 200) {
+      return callback(new Error('tests/test-api.js -API Error: ' + res.statusCode));
+    }
+
+    return callback(null, res, body);
+  });
+};
+
+/**
+ * Test 6: Mock gameover + chats
+ */
+
+var test6 = function(callback) {
+
+  mock.statsHeaders6.sessionid = sessionid;
+  optionsGameover.headers = mock.statsHeaders6;
+  optionsGameover.json = mock.statsBody6;
+
+  request(optionsGameover, function(err, res, body) {
+    // Error checking
+    if (err) {
+      return callback(err);
+    }
+    if (res.statusCode !== 200) {
+      return callback(new Error('tests/test-api.js -API Error: ' + res.statusCode));
+    }
+
+    return callback(null, res, body);
+  });
+};
+
+
+
+// Run tests
+async.series([
+  function(callback) {
+    test1(function(err, res, body) {
+      assert.ok(body, 'body should return true');
+      assert.ok(res.headers.matchurl, 'matchurl should be present');
+      assert.ok(res.headers.sessionid, 'sessionid should be present');
+      callback(err);
+    });
+  }
+
+, function(callback) {
+    test2(function(err, res, body) {
+      assert.ok(body, 'body should return true');
+      assert.ok(!res.headers.matchurl, 'matchurl should not be present');
+      assert.ok(!res.headers.sessionid, 'sessionid should not be present');
+      callback(err);
+    });
+  }
+
+, function(callback) {
+    test3(function(err, res, body) {
+      assert.ok(body, 'body should return true');
+      assert.ok(!res.headers.matchurl, 'matchurl should not be present');
+      assert.ok(!res.headers.sessionid, 'sessionid should not be present');
+      callback(err);
+    });
+  }
+
+, function(callback) {
+    test4(function(err, res, body) {
+      assert.ok(body, 'body should return true');
+      assert.ok(!res.headers.matchurl, 'matchurl should not be present');
+      assert.ok(!res.headers.sessionid, 'sessionid should not be present');
+      callback(err);
+    });
+  }
+
+, function(callback) {
+    test5(function(err, res, body) {
+      assert.ok(body, 'body should return true');
+      assert.ok(!res.headers.matchurl, 'matchurl should not be present');
+      assert.ok(!res.headers.sessionid, 'sessionid should not be present');
+      callback(err);
+    });
+  }
+
+, function(callback) {
+    test6(function(err, res, body) {
+      assert.ok(body, 'body should return true');
+      assert.ok(!res.headers.matchurl, 'matchurl should not be present');
+      assert.ok(!res.headers.sessionid, 'sessionid should not be present');
+      callback(err);
+    });
+  }
+
+], function(err, results) {
+  if (err) {
+    console.log(err);
+    console.trace(err);
+  } else {
+    console.log('test-api.js successfully completed.');
+  }
+});
