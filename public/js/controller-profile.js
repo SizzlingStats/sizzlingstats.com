5d0ebce07b36bc78a0717c046d57984bfe7eadc3
Christopher Kim <christopherjkim@gmail.com>
2013-02-15 02:37:33 -0500

    The best fucking pagination the world has ever seen.

--- public/js/controller-profile.js
+++ public/js/controller-profile.js
@@ -3,6 +3,8 @@ function ProfileCtrl($scope, $rootScope, $location, $http, socket, resolvedData)
     // do something
   }
   var PAGE_SIZE = 10;
+  var NUM_ADJACENT_PAGES = 2;
+  var MAX_NUM_PAGES = 9;
 
   $scope.player = resolvedData.player;
   $scope.player.avatarFull = $scope.player.avatar.slice(0, -4) + '_full.jpg';
@@ -10,28 +12,79 @@ function ProfileCtrl($scope, $rootScope, $location, $http, socket, resolvedData)
   
   $scope.count = resolvedData.count;
   $scope.pageNumCurrent = 1;
-  $scope.pageNumLast = Math.ceil( $scope.count / PAGE_SIZE );
+  $scope.pageNumLast = Math.floor( $scope.count / PAGE_SIZE + 1);
+  $scope.pages = [];
 
-  $scope.pages = [{num: 1, isCurrent: true}];
-  for (var i=2; i<=$scope.pageNumLast; i++) {
-    $scope.pages.push({
-      num: i
-    });
+
+  function Page(num) {
+    this.num = num;
+    this.isCurrent = function() {
+      return this.num === $scope.pageNumCurrent;
+    };
   }
 
+  // Build the pagination ul
+  var buildPagination = function() {
+    $scope.pages = [];
+    if ($scope.pageNumLast <= MAX_NUM_PAGES) {
+      // Not enough pages to bother breaking it up
+      for (var i=1; i<=$scope.pageNumLast; i++) {
+        $scope.pages.push(new Page(i));
+      }
+    } else if ($scope.pageNumCurrent + NUM_ADJACENT_PAGES <= MAX_NUM_PAGES - 2) {
+      // Close to beginning; only hide later pages
+      for (var i=1; i<=MAX_NUM_PAGES-2; i++) {
+        $scope.pages.push(new Page(i));
+      }
+      // $scope.pages.push(new Page('&hellip;'));
+      $scope.pages.push(new Page('…'));
+      $scope.pages.push(new Page($scope.pageNumLast));
+    } else if ($scope.pageNumLast - $scope.pageNumCurrent + 1 + NUM_ADJACENT_PAGES <= MAX_NUM_PAGES - 2) {
+      // Close to end; only hide earlier pages
+      $scope.pages.push(new Page(1));
+      $scope.pages.push(new Page('…'));
+      for (var i=$scope.pageNumLast - (MAX_NUM_PAGES - NUM_ADJACENT_PAGES*2 + 1); i<=$scope.pageNumLast; i++) {
+        $scope.pages.push(new Page(i));
+      }
+    } else {
+      // In the middle; hide earlier and later pages
+      $scope.pages.push(new Page(1));
+      $scope.pages.push(new Page('…'));
+      for (var i=$scope.pageNumCurrent-NUM_ADJACENT_PAGES; i<=$scope.pageNumCurrent+NUM_ADJACENT_PAGES; i++) {
+        $scope.pages.push(new Page(i));
+      }
+      $scope.pages.push(new Page('…'));
+      $scope.pages.push(new Page($scope.pageNumLast));
+    }
+  };
+  buildPagination();
 
   $scope.clickPageNumber = function(index) {
-    if ($scope.pages[index].isCurrent) { return; }
+    if (this.page.isCurrent()) { return; }
+    if (this.page.num === '…') {
+      // BINARY SEARCH WOOOOO
+      goToPage(Math.floor( ( $scope.pages[index+1].num + $scope.pages[index-1].num ) / 2 ));
+    } else {
+      goToPage(this.page.num);
+    }
+  };
+  $scope.clickPageLeft = function() {
+    goToPage($scope.pageNumCurrent-1);
+  };
+  $scope.clickPageRight = function() {
+    goToPage($scope.pageNumCurrent+1);
+  };
+
+  var goToPage = function(pageNum) {
+    if (pageNum < 1 || pageNum > $scope.pageNumLast) { return false; }
     $http.get('/api/player/' + $scope.player._id + '/matches?currentmatch=' +
-              $scope.matches[0]._id + '&skip=' + ($scope.pages[index].num - $scope.pageNumCurrent) * 10 )
+              $scope.matches[0]._id + '&skip=' + (pageNum - $scope.pageNumCurrent) * PAGE_SIZE )
     .success(function(data, status, headers, config) {
         $scope.matches = data.matches;
-        $scope.pages[$scope.pageNumCurrent-1].isCurrent = false;
-        $scope.pages[index].isCurrent = true;
-        $scope.pageNumCurrent = $scope.pages[index].num;
+        $scope.pageNumCurrent = pageNum;
+        buildPagination();
     }).error(function(data, status, headers, config) {
       // Do something
-        // $scope.status = status;
     });
   };
 
