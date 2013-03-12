abcb2a0d94a8539b300960d67163296f6297e628
Christopher Kim <christopherjkim@gmail.com>
2013-03-12 02:16:06 -0400

    Removed legacy `app.configure()` method.

--- app.js
+++ app.js
@@ -120,151 +120,138 @@ everyauth.debug = false;
  * Express Configuration
  */
 
-app.configure('development', function() {
-  // app.use(express.profiler());
-  // app.use(express.logger({ format: 'dev' }));
+app.enable('trust proxy');
+app.use(express.limit('200kb'));
+app.use(express.favicon(__dirname + '/public/img/favicon.png'
+                      , { maxAge: 14 * 24 * 60 * 60 * 1000 } ));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', {
+  layout: false
+});
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.use(analytics);
+
+// Sessions
+
+// app.store = new express.session.MemoryStore;
+var RedisStore = require('connect-redis')(express);
+app.store = new RedisStore({
+  prefix: cfg.session_prefix
+, host: cfg.redis_host
+, port: cfg.redis_port
+, db: cfg.redis_db
+, pass: cfg.redis_password
 });
 
-app.configure(function() {
-  app.enable('trust proxy');
-  app.use(express.limit('200kb'));
-  app.use(express.favicon(__dirname + '/public/img/favicon.png'
-                        , { maxAge: 14 * 24 * 60 * 60 * 1000 } ));
-
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('view options', {
-    layout: false
-  });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-
-  app.use(analytics);
-
-  // Sessions
-
-  // app.store = new express.session.MemoryStore;
-  var RedisStore = require('connect-redis')(express);
-  app.store = new RedisStore({
-    prefix: cfg.session_prefix
-  , host: cfg.redis_host
-  , port: cfg.redis_port
-  , db: cfg.redis_db
-  , pass: cfg.redis_password
-  });
-
-  app.use(express.cookieParser());
-  app.use(express.session({
-    proxy: true
-  , store: app.store
-  , secret: secrets.session
-  , cookie: {
-      path: '/'
-    , httpOnly: true
-    // cookies expire every 14 days
-    , maxAge: 14 * 24 * 60 * 60 * 1000
-    }
-  }));
-  app.use(everyauth.middleware());
+app.use(express.cookieParser());
+app.use(express.session({
+  proxy: true
+, store: app.store
+, secret: secrets.session
+, cookie: {
+    path: '/'
+  , httpOnly: true
+  // cookies expire every 14 days
+  , maxAge: 14 * 24 * 60 * 60 * 1000
+  }
+}));
+app.use(everyauth.middleware());
 
 
-  // Asset Management
+// Asset Management
 
-  var assetManager = require('connect-assetmanager')({
-    js: {
-      route: /\/js\/all-[a-z0-9]+\.js/
-    , path: __dirname + '/public/js/'
-    , dataType: 'javascript'
-    , debug: process.env.NODE_ENV === 'development'
-    // , preManipulate: {
-    //     '^': [
-    //         function(src, path, index, isLast, callback) {
-    //           callback(src.replace(/#socketIoPort#/g, env.port));
-    //         }
-    //       , function(src, path, index, isLast, callback) {
-    //           if (/\.coffee$/.test(path)) {
-    //             callback(coffee.compile(src));
-    //           } else {
-    //             callback(src);
-    //           }
-    //         }
-    //     ]
-    //   }
-    , files: [ // order matters here
-      //   'lib/jquery/jquery-1.9.1.min.js'
-      // , 'lib/angular/angular.js'
-      // , 'lib/foundation/foundation.min.js'
-        'lib/typeahead.js'
-      , 'lib/foundation/modernizr.foundation.js'
-      // , 'lib/foundation/jquery.foundation.mediaQueryToggle.js'
-      // , 'lib/foundation/jquery.foundation.navigation.js'
-      , 'lib/foundation/jquery.foundation.tooltips.js'
-      , 'lib/foundation/jquery.foundation.topbar.js'
-      , 'lib/foundation/app.js'
-      , 'app.js'
-      , 'services.js'
-      , 'controllers.js'
-      , 'controller-stats.js'
-      , 'controller-player.js'
-      , 'controller-profile.js'
-      // , 'filters.js'
-      , 'directives.js'
-      , 'directive-typeahead.js'
-      // , '*'
-      ]
-    },
-    css: {
-      route: /\/css\/all-[a-z0-9]+\.css/
-    , path: __dirname + '/public/css/'
-    , dataType: 'css'
-    , debug: process.env.NODE_ENV === 'development'
-      // preManipulate: {
-      //   '^': [
-      //     function(src, path, index, isLast, callback) {
-      //       if (/\.styl$/.test(path)) {
-      //         stylus(src)
-      //           .set('compress', false)
-      //           .set('filename', path)
-      //           .set('paths', [ __dirname, app.paths.public ])
-      //           .render(function(err, css) {
-      //             callback(err || css);
-      //           });
-      //       } else {
-      //         callback(src);
-      //       }
-      //     }
-      //   ]
-      // },
-    , files: [ // order matters here
-        'foundation.css'
-      , 'typeahead.css'
-      , 'sizzlingstats.css'
-      ]
-    }
-  });
-  app.use(assetManager);
-  // app.helpers({ assetManager: assetManager });
-  app.locals.assetManager = assetManager;
-  app.use(express.staticCache());
+var assetManager = require('connect-assetmanager')({
+  js: {
+    route: /\/js\/all-[a-z0-9]+\.js/
+  , path: __dirname + '/public/js/'
+  , dataType: 'javascript'
+  , debug: process.env.NODE_ENV === 'development'
+  // , preManipulate: {
+  //     '^': [
+  //         function(src, path, index, isLast, callback) {
+  //           callback(src.replace(/#socketIoPort#/g, env.port));
+  //         }
+  //       , function(src, path, index, isLast, callback) {
+  //           if (/\.coffee$/.test(path)) {
+  //             callback(coffee.compile(src));
+  //           } else {
+  //             callback(src);
+  //           }
+  //         }
+  //     ]
+  //   }
+  , files: [ // order matters here
+    //   'lib/jquery/jquery-1.9.1.min.js'
+    // , 'lib/angular/angular.js'
+    // , 'lib/foundation/foundation.min.js'
+      'lib/typeahead.js'
+    , 'lib/foundation/modernizr.foundation.js'
+    // , 'lib/foundation/jquery.foundation.mediaQueryToggle.js'
+    // , 'lib/foundation/jquery.foundation.navigation.js'
+    , 'lib/foundation/jquery.foundation.tooltips.js'
+    , 'lib/foundation/jquery.foundation.topbar.js'
+    , 'lib/foundation/app.js'
+    , 'app.js'
+    , 'services.js'
+    , 'controllers.js'
+    , 'controller-stats.js'
+    , 'controller-player.js'
+    , 'controller-profile.js'
+    // , 'filters.js'
+    , 'directives.js'
+    , 'directive-typeahead.js'
+    // , '*'
+    ]
+  },
+  css: {
+    route: /\/css\/all-[a-z0-9]+\.css/
+  , path: __dirname + '/public/css/'
+  , dataType: 'css'
+  , debug: process.env.NODE_ENV === 'development'
+    // preManipulate: {
+    //   '^': [
+    //     function(src, path, index, isLast, callback) {
+    //       if (/\.styl$/.test(path)) {
+    //         stylus(src)
+    //           .set('compress', false)
+    //           .set('filename', path)
+    //           .set('paths', [ __dirname, app.paths.public ])
+    //           .render(function(err, css) {
+    //             callback(err || css);
+    //           });
+    //       } else {
+    //         callback(src);
+    //       }
+    //     }
+    //   ]
+    // },
+  , files: [ // order matters here
+      'foundation.css'
+    , 'typeahead.css'
+    , 'sizzlingstats.css'
+    ]
+  }
 });
+app.use(assetManager);
+// app.helpers({ assetManager: assetManager });
+app.locals.assetManager = assetManager;
+app.use(express.staticCache());
 
-app.configure('development', function() {
+if (app.get('env') === 'development') {
   app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('production', function() {
+} else {
   app.use(express.static(__dirname + '/public', {maxAge: 24 * 60 * 60 * 1000}));
-});
+}
 
-app.configure(function() {
-  app.use(app.router);
-});
+app.use(app.router);
 
-app.configure('development', function() {
+if (app.get('env') === 'development') {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function() {
+} else {
   // Airbrake
   app.use(function(err, req, res, next) {
     airbrake.expressHandler().apply(this, arguments);
@@ -274,7 +261,7 @@ app.configure('production', function() {
     res.set('Content-Type', 'text/plain');
     res.send(500, "that's a meatshot" );
   });
-});
+}
 
 // Routes
 require('./routes')(app);
